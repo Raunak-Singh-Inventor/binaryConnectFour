a10b5317558283742f785069baa4d949e60b28fb
raunak-singh-inventor <raunak.singh.inventor@gmail.com>
2022-02-01 20:26:34 -0500

    added some prints

--- main.cpp
+++ main.cpp
@@ -5,7 +5,31 @@ using namespace std;
 
 int main()
 {
+    cout << "------------------------------------------------------------------" << endl;
+    cout << "| Welcome to binaryConnect4!                                     |" << endl;
+    cout << "| this is an electronic version of traditional connect4          |" << endl;
+    cout << "| instead of red and yellow chips, binaryConnect4 uses 1s and 0s |" << endl;
+    cout << "------------------------------------------------------------------" << endl;
+
+
+    char response=' ';
+    while(true) {
+        cout << "Would you like to start a new game? (y/n): ";
+        cin >> response;
+        if(response=='y') {
+            break;
+        } else if(response=='n') {
+            cout << "bye this software will stalk you";
+            return 0;
+        } else {
+            cout << "input 'y' or 'n'" << endl;
+        }
+    }
+
     Matrix board = Matrix();
     board.printMatrix();
+
+
+
     return 0;
 }
